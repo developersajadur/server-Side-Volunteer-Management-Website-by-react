3fff3a4948d4444afde82ea8976782827c9b0d34
itzmesojib <itzmesojib@gmail.com>
2024-05-14 13:46:40 +0600

    private mongo db secret key and jwt secrert

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
-const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-// ------------------- middleware ------------------------
-const corsOptions ={
-  origin: [
-    "http://localhost:5173",
-  ],
+const port = process.env.PORT || 5000;
+
+// ------------------- Middleware ------------------------
+const corsOptions = {
+  origin: ["http://localhost:5173"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
@@ -18,198 +18,256 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-// ------------- verify token middleware --------------------
-const verifyToken = async(req, res, next) => {
-     const token = req?.cookies?.token;
-     if(!token){
-      return res.status(401).send({message: "unAuthorized Access denied"})
-     }
-      console.log(token);
-      if(token){
-        jwt.verify(token, "Sojib@123", (err, decoded) => {
-          if(err){
-            return res.status(401).send({message: "unAuthorized Access denied"})
-          }
-          console.log(decoded);
-          req.user = decoded;
-          next();
-        });
-      }
+// ------------- Verify Token Middleware --------------------
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access denied" });
+  }
+  jwt.verify(token, `${process.env.JWT_TOKEN_SECRET}`, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access denied" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
-}
+// ---------------------- Routes ---------------------------
 
 app.get("/", (req, res) => {
-    res.send("VolunteerHub Server Is Running");
+  res.send("VolunteerHub Server Is Running");
 });
 
-// ---------------------------------------------------------------
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteerhub.pjcv7iq.mongodb.net/?retryWrites=true&w=majority&appName=VolunteerHub`;
 
-const uri = "mongodb+srv://VolunteerHub:rIEdfqbg8m2Bl9S8@volunteerhub.pjcv7iq.mongodb.net/?retryWrites=true&w=majority&appName=VolunteerHub";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    // ------------------------------ databases Collections ------------------------------
-    const VolunteerCollections = client.db("VolunteerHub").collection("VolunteerCollections");
-    const jobRequestCollections = client.db("VolunteerHub").collection("JobRequestCollections");
+    const VolunteerCollections = client
+      .db("VolunteerHub")
+      .collection("VolunteerCollections");
+    const jobRequestCollections = client
+      .db("VolunteerHub")
+      .collection("JobRequestCollections");
 
-    // ------------------- jwt token generator ----------------
-    app.post("/jwt", async(req, res) => {
+    // ------------------- JWT Token Generation ----------------
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, "Sojib@123",{
-        expiresIn: "7d"
+      const token = jwt.sign(user, "Sojib@123", {
+        expiresIn: "7d",
       });
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-
-    }).send({success: true});
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
-    // -------------------------- jwt refresh token--------------------------
+    // -------------------------- Logout Route --------------------------
     app.post("/logout", async (req, res) => {
-      res.clearCookie("Token", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite:  process.env.NODE_ENV === "production"? "none" : "strict", 
-        maxAge: 0,
-      }).send({success: true});
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
     });
 
-    // ----------------------------------- post all VolunteerCollections --------------------------------
-    app.post("/volunteers-post", async (req, res) =>{
+    // --------------------------------- Post Volunteer Collections ---------------------------------
+    app.post("/volunteers-post", async (req, res) => {
       const VolunteerCollectionsData = req.body;
-      const result = await VolunteerCollections.insertOne(VolunteerCollectionsData);
+      const result = await VolunteerCollections.insertOne(
+        VolunteerCollectionsData
+      );
       res.send(result);
     });
 
-    // --------------------------- get all VolunteerCollections post --------------------------
+    // ----------------------------- Get all Volunteer Collections -----------------------------
     app.get("/volunteers-post", async (req, res) => {
       const option = {
-          projection: { thumbnail: 1, postTitle: 1, category: 1, volunteersNeeded: 1, deadline: 1, location: 1 }
+        projection: {
+          thumbnail: 1,
+          postTitle: 1,
+          category: 1,
+          volunteersNeeded: 1,
+          deadline: 1,
+          location: 1,
+        },
       };
-      const VolunteerCollectionsData = await VolunteerCollections.find({}, option).toArray();
+      const VolunteerCollectionsData = await VolunteerCollections.find(
+        {},
+        option
+      ).toArray();
       res.send(VolunteerCollectionsData);
     });
 
-    // -------------------------------- update post ----------------------------
+    // --------------------------------- Update Post ---------------------------------
     app.put("/volunteers-post/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatePost = req.body;
-      const post ={
-        $set:{
+      const post = {
+        $set: {
           thumbnail: updatePost.thumbnail,
           postTitle: updatePost.postTitle,
           category: updatePost.category,
           volunteersNeeded: updatePost.volunteersNeeded,
           deadline: updatePost.deadline,
           location: updatePost.location,
-          description: updatePost.description
-        }
+          description: updatePost.description,
+        },
       };
-      const result = await VolunteerCollections.updateOne(filter, post, options);
+      const result = await VolunteerCollections.updateOne(
+        filter,
+        post,
+        options
+      );
       res.send(result);
     });
 
-    // -------------------------- get single post ------------------------------
+    // -------------------------- Get Single Post ------------------------------
     app.get("/volunteers-post/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const VolunteerCollectionsData = await VolunteerCollections.findOne(query);
+      const query = { _id: new ObjectId(id) };
+      const VolunteerCollectionsData = await VolunteerCollections.findOne(
+        query
+      );
       res.send(VolunteerCollectionsData);
     });
 
-    // ---------------------------- delete a Volunteer post ----------------------------
+    // -------------------------- Delete a Volunteer Post ------------------------------
     app.delete("/volunteers-post/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await VolunteerCollections.deleteOne(query);
       res.send(result);
     });
 
-    // --------------------------- get all VolunteerCollections by email --------------------------
-    app.get("/volunteers-post/user/:email", verifyToken, async(req, res) => {
+    // ------------------------- Get all Volunteer Collections by email -------------------------
+    app.get("/volunteers-post/user/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const tokenEmail = req.user.email;
-      if(tokenEmail !== email){
-        return res.status(403).send({message: "Forbidden Access denied"})
+      if (tokenEmail !== email) {
+        return res.status(403).send({ message: "Forbidden Access denied" });
       }
-      const VolunteerCollectionsData = await VolunteerCollections.find({ "email": email }).toArray();
+      const VolunteerCollectionsData = await VolunteerCollections.find({
+        email: email,
+      }).toArray();
       res.send(VolunteerCollectionsData);
     });
 
-    // ----------------------------------- get volunteer details --------------------------------
+    // -------------------------------- Get Volunteer Details --------------------------------
     app.get("/volunteer-details/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const VolunteerCollectionsData = await VolunteerCollections.findOne(query);
+      const query = { _id: new ObjectId(id) };
+      const VolunteerCollectionsData = await VolunteerCollections.findOne(
+        query
+      );
       res.send(VolunteerCollectionsData);
     });
 
-    // ---------------------- post job request --------------------
+    // --------------------------- Post Job Request ---------------------------
     app.post("/my-request-job", async (req, res) => {
       const request = req.body;
       const result = await jobRequestCollections.insertOne(request);
       res.send(result);
     });
 
-    // --------------------- get all jobs request --------------------
+    // ------------------------- Get all Jobs Request -------------------------
     app.get("/my-request-job", async (req, res) => {
-      const jobRequestCollectionsData = await jobRequestCollections.find({}).toArray();
+      const jobRequestCollectionsData = await jobRequestCollections
+        .find({})
+        .toArray();
       res.send(jobRequestCollectionsData);
     });
 
-    // --------------------- get job request by email --------------------
-    app.get("/my-request-job/:email", verifyToken, async (req, res) => {
+    // ------------------------- Get Job Request by email -------------------------
+    app.get("/my-request-job/:email", async (req, res) => {
       const email = req.params.email;
-      const tokenEmail = req.user.email;
-      if(tokenEmail !== email){
-        return res.status(403).send({message: "Forbidden Access denied"})
-      }
-      const jobRequestCollectionsData = await jobRequestCollections.find({ email: email }).toArray();
+      // }
+      const jobRequestCollectionsData = await jobRequestCollections
+        .find({ email: email })
+        .toArray();
       res.send(jobRequestCollectionsData);
     });
 
-    // --------------------- get job request by email for post admin --------------------
-    app.get("/request-job/:email",verifyToken, async (req, res) => {
+    // ------------------------- Get Job Request by email for post admin -------------------------
+    app.get("/request-job/:email", async (req, res) => {
       const email = req.params.email;
-      const tokenEmail = req.user.email;
-      if(tokenEmail !== email){
-        return res.status(403).send({message: "Forbidden Access denied"})
-      }
-      const jobRequestCollectionsData = await jobRequestCollections.find({ "postAdminEmail": email }).toArray();
+      const jobRequestCollectionsData = await jobRequestCollections
+        .find({ postAdminEmail: email })
+        .toArray();
       res.send(jobRequestCollectionsData);
     });
 
-    // ----------------- job request status change ----------------
+    // -------------------- Get all Volunteer Collections for pagination ---------------------
+    app.get("/all-volunteers-post", async (req, res) => {
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const search = req.query.search;
+    
+      let query = {};
+      if (search) {
+        query = {
+          postTitle: { $regex: search, $options: "i" }
+        };
+      }
+    
+      try {
+        const VolunteerCollectionsData = await VolunteerCollections.find(query)
+          .skip(size * page)
+          .limit(size)
+          .toArray();
+        res.send(VolunteerCollectionsData);
+      } catch (error) {
+        console.error("Error fetching volunteer posts:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+    
+
+    // ----------------------------- Get all Volunteer Collections -----------------------------
+    app.get("/all-volunteers-post-count", async (req, res) => {
+      const count = await VolunteerCollections.countDocuments();
+      res.send({ count });
+    });
+
+    // ------------------------- Job Request Status Change -------------------------
     app.patch("/job/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const status = req.body;
       const updateStatus = {
-        $set: status
+        $set: status,
       };
-      const result = await jobRequestCollections.updateOne(filter, updateStatus);
+      const result = await jobRequestCollections.updateOne(
+        filter,
+        updateStatus
+      );
       res.send(result);
     });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -217,8 +275,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-// ---------------------------------------------------------------
-
+// Start the server
 app.listen(port, () => {
-    console.log(`VolunteerHub Server is running on port ${port}`);
+  console.log(`VolunteerHub Server is running on port ${port}`);
 });
